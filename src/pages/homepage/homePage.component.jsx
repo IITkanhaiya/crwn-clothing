beda8ca6c46a474c735a8763ee102b2d19d86395
kanhaiya <klalsharma.phe18@itbhu.ac.in>
2021-09-22 00:58:13 +0530

    update our menu-item to navigatre withRouter HOC

---
+++ src/pages/homepage/homePage.component.jsx
@@ -0,0 +1,11 @@
+import react from "react";
+import Directory from '../../components/directory/directory.component';
+import './homePage.style.scss';
+
+const HomePage= ()=>(
+    <div className='homepage'>
+       <Directory/>
+    </div>
+
+);
+export default HomePage;
